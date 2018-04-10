8abc1f0c403903fa6eb9bd1ee5d82c29ac6014bc
georgioslamprakis <george.lamprakis@tesco.com>
2018-04-10 17:06:37 +0100

    Common items  first refactoring circle

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,9 @@
 package com.gildedrose;
 
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
+
 class GildedRose {
     Item[] items;
 
@@ -7,8 +11,20 @@ class GildedRose {
     private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private final String BACKSTAGE_TICKETS = "Backstage passes to a TAFKAL80ETC concert";
 
+    private final List<String> specialItems = new LinkedList<>();
+
+
     public GildedRose(Item[] items) {
         this.items = items;
+        populateSpecialItems();
+
+    }
+
+    private void populateSpecialItems(){
+        specialItems.add(AGED_BRIE);
+        specialItems.add(SULFURAS);
+        specialItems.add(BACKSTAGE_TICKETS);
+
     }
 
     public void updateQuality() {
@@ -38,6 +54,17 @@ class GildedRose {
             }
 
             if(items[i].name.equals(SULFURAS)){
+                continue;
+            }
+
+            if(!specialItems.contains(items[i].name)){
+                items[i].sellIn--;
+                if (items[i].quality > 0) {
+                    items[i].quality--;
+                }
+                if (items[i].quality > 0 && items[i].sellIn < 0){
+                    items[i].quality--;
+                }
                 continue;
             }
 
